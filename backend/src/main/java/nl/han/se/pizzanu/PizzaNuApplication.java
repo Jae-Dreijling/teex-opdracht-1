d25a390b0789e97cc258d82f9bac4bfa8f384935
Jaap Papavoine <jaap.papavoine@han.nl>
2023-09-15 10:29:24 +0200

    add CORS

--- backend/src/main/java/nl/han/se/pizzanu/PizzaNuApplication.java
+++ backend/src/main/java/nl/han/se/pizzanu/PizzaNuApplication.java
@@ -2,12 +2,25 @@ package nl.han.se.pizzanu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PizzaNuApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PizzaNuApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
