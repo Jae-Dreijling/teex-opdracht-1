1bc4228e4eb900d1601e41efd7725c4ee37ebeea
Jaap Papavoine <jaap.papavoine@han.nl>
2023-09-15 10:30:24 +0200

    addapt sql schema to productname

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,5 +1,8 @@
-INSERT INTO `pizzanu`.`pizzas` (`name`, `description`) VALUES ('Margerita', 'Kaas, Tomaat, Oregano');
-INSERT INTO `pizzanu`.`pizzas` (`name`, `description`) VALUES ('Fungi', 'Kaas, Tomaat, Champignon');
-INSERT INTO `pizzanu`.`pizzas` (`name`, `description`) VALUES ('Hawaii', 'Kaas, Tomaat, Ham, Ananas');
-INSERT INTO `pizzanu`.`pizzas` (`name`, `description`) VALUES ('Quattro Staggione', 'All the things');
-
+INSERT INTO `pizzanu`.`pizzas`  (`productname`,         `description`,                  `price`, `ordercount`)
+VALUES                          ('Margerita',           'Kaas, Tomaat, Oregano',        10,      0);
+INSERT INTO `pizzanu`.`pizzas`  (`productname`,         `description`,                  `price`,     `ordercount`)
+VALUES                          ('Fungi',               'Kaas, Tomaat, Champignon',     10,      0);
+INSERT INTO `pizzanu`.`pizzas`  (`productname`,         `description`,                  `price`, `ordercount`)
+VALUES                          ('Hawaii',              'Kaas, Tomaat, Ham, Ananas',    10,      0);
+INSERT INTO `pizzanu`.`pizzas`  (`productname`,         `description`,                  `price`, `ordercount`)
+VALUES                          ('Quattro Staggione', 'All the things',                 10,      0);
--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,14 +1,12 @@
 DROP TABLE IF EXISTS `pizzanu`.`pizzas`;
-DROP TABLE IF EXISTS `pizzanu`.`pizzas`;
-DROP TABLE IF EXISTS `pizzanu`.`pizzas`;
-DROP TABLE IF EXISTS `pizzanu`.`pizzas`;
-DROP TABLE IF EXISTS `pizzanu`.`pizzas`;
 
 CREATE TABLE `pizzas`
 (
-    `id`          bigint      NOT NULL AUTO_INCREMENT,
-    `name`       varchar(255) DEFAULT NULL,
+    `id`          bigint       NOT NULL AUTO_INCREMENT,
+    `productname` varchar(255) DEFAULT NULL,
     `description` varchar(255) DEFAULT NULL,
+    `price`       bigint       DEFAULT NULL,
+    `ordercount`  bigint       DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
