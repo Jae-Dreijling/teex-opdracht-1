991a273e0ba0bc51e2a700fce827efc2266760ba
Jaap Papavoine <jaap.papavoine@han.nl>
2023-09-20 15:28:31 +0200

    Moar Spring websocket stuff

--- backend/src/main/java/nl/han/se/pizzanu/WebSocketConfig.java
+++ backend/src/main/java/nl/han/se/pizzanu/WebSocketConfig.java
@@ -1,2 +1,14 @@
-package nl.han.se.pizzanu;public class WebSocketConfig {
+package nl.han.se.pizzanu;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new SocketHandler(), "/name");
+    }
 }
